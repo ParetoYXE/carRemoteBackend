bddd232daabe7d3c85e3f17c3ec948d610aca1d1
Liam <paretoyxe@gmail.com>
2020-09-16 13:11:42 -0700

    ready to deploy

--- app.js
+++ app.js
@@ -1,75 +1,15 @@
-//Liam Iverson
-//EVOLVO
-//ExpressJS Server and Database Wrappe
-
-
-const express = require('express')
-const bodyParser = require('body-parser')
+// create an express app
+const express = require("express")
 const app = express()
-const port = 3000
-const MongoClient = require('mongodb').MongoClient
 
+// use the express-static middleware
+app.use(express.static("public"))
 
-//Database connection string
-
-conString = "mongodb+srv://admin:evolvo@cluster0.kzsnt.mongodb.net/carData?retryWrites=true&w=majority"
-
-app.use(bodyParser.urlencoded({extended : true}))
-app.use(bodyParser.json())
-
-
-
-//Load and QueryDataBase
-MongoClient.connect(conString,{ useUnifiedTopology: true }).then(client =>{
-
-    const db = client.db('CAR')
-    const quotesCollection = db.collection('DASH')
-
-    //Load Initial Database
-    const cursor = db.collection('DASH').find().toArray()
-		.then(results =>{
-			console.log(results)
-		}).catch(error => console.error(error))
-
-
-    //Writes to DB everytime the form is updated
-    app.post('/car', (req, res) => {
-	  quotesCollection.insertOne(req.body)
-	    .then(result => {
-	      res.redirect('/')
-	    })
-	    .catch(error => console.error(error))
-	})
-
-    //Loads index and on refresh reads from the DB
-	app.get('/', (req, res) => {
-		res.sendFile(__dirname + '/index.html')
-		const cursor = db.collection('DASH').find().toArray()
-		.then(results =>{
-			console.log(results)
-		}).catch(error => console.error(error))
-	})
-
-
-	//When the Server recieves data it writes to the db
-	app.put('/car',(req,res)=>{
-		quotesCollection.insertOne(req.body)
-		res.sendFile(__dirname + '/index.html')
-		const cursor = db.collection('DASH').find().toArray()
-		.then(results =>{
-			console.log(results)
-		}).catch(error => console.error(error))
-	})
-
-	})
-
-
-
-
-//Tell Server to begin listening on PORT 3000
-app.listen(port, () => {
-  console.log(`Example app listening at http://localhost:${port}`)
+// define the first route
+app.get("/", function (req, res) {
+  res.send("<h1>Hello World!</h1>")
 })
 
-
-app.use(express.static('public'))
+// start the server listening for requests
+app.listen(process.env.PORT || 3000, 
+	() => console.log("Server is running..."));
