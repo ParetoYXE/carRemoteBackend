7de690c3d049ec27a6f777077b22a8514ca78bee
ParetoYXE <35578020+ParetoYXE@users.noreply.github.com>
2020-09-16 16:33:00 -0700

    Update app.js

--- app.js
+++ app.js
@@ -1,12 +1,31 @@
 var express = require('express');
+const bodyParser = require('body-parser')
 var app = express();
 
-app.get('/', function(req, res){
-   res.send("Hello world!");
-});
 
-app.get('/car',function(req,res){
-	res.send("Car data requested.")
+const MongoClient = require('mongodb').MongoClient
+
+
+conString = "mongodb+srv://admin:evolvo@cluster0.kzsnt.mongodb.net/carData?retryWrites=true&w=majority"
+
+app.use(bodyParser.urlencoded({extended : true}))
+app.use(bodyParser.json())
+
+
+MongoClient.connect(conString,{ useUnifiedTopology: true }).then(client =>{
+
+    const db = client.db('CAR')
+    const quotesCollection = db.collection('DASH')
+
+	app.get('/', function(req, res){
+	   res.send("Hello world!");
+	});
+
+	app.get('/car',function(req,res){
+		res.send("Car data requested.")
+	})
+
+
+
 })
-
 app.listen(3000);
