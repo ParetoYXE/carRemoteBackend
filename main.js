9aae163cdead9ee0bdc4030d2a42dce428c8a891
Liam <paretoyxe@gmail.com>
2020-09-16 13:02:59 -0700

    Make it so

---
+++ main.js
@@ -0,0 +1,13 @@
+const update = document.querySelector('#update-button')
+
+update.addEventListener('click', _ => {
+  fetch('/car', {
+    method: 'put',
+    headers: { 'Content-Type': 'application/json' },
+    body: JSON.stringify({
+      name: 'Voltage',
+      quote: '12000mV'
+    })
+  })
+})
+
