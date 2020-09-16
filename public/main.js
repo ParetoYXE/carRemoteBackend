773b75463f7b3d7394b22f8aade1cc21420cb3a6
ParetoYXE <35578020+ParetoYXE@users.noreply.github.com>
2020-09-16 15:51:04 -0700

    Update main.js

--- public/main.js
+++ public/main.js
@@ -1,7 +1,7 @@
 const update = document.querySelector('#update-button')
 
 update.addEventListener('click', _ => {
-  fetch('/car', {
+  fetch('http://138.197.149.50:3000/car', {
     method: 'put',
     headers: { 'Content-Type': 'application/json' },
     body: JSON.stringify({
