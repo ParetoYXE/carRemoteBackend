6f5ca455d8571dda15481185758a64bbf1ff4244
ParetoYXE <35578020+ParetoYXE@users.noreply.github.com>
2020-09-16 16:59:39 -0700

    Update app.js

--- app.js
+++ app.js
@@ -18,16 +18,17 @@ MongoClient.connect(conString,{ useUnifiedTopology: true }).then(client =>{
     const quotesCollection = db.collection('DASH')
 
 
-	app.get('/', function(req, res){
-		   res.send("Hello world!");
-		});
-
-	app.get('/car',function(req,res){
-		
-		res.send("Car data requested.")
-	})
+})
 
 
+
+
+app.get('/', function(req, res){
+	res.send("Hello world!");
+});
+
+app.get('/car',function(req,res){
+	res.send("Car data requested.")
 })
 
 
