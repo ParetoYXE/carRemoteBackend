5ecede00b2bc8ed821b718d4eb0d2028862e8288
ParetoYXE <35578020+ParetoYXE@users.noreply.github.com>
2020-09-17 11:35:03 -0700

    Update app.js

--- app.js
+++ app.js
@@ -11,22 +11,24 @@ conString = "mongodb+srv://admin:evolvo@cluster0.kzsnt.mongodb.net/carData?retry
 app.use(bodyParser.urlencoded({extended : true}))
 app.use(bodyParser.json())
 
-data = "Test Data"
-
-MongoClient.connect(conString,{ useUnifiedTopology: true }).then(client =>{
-	data = "MongoDB Connected"
 
 
-	app.get('/', function(req, res){
-	   res.send(data);
+
+app.get('/', function(req, res){
+	   testFunction()
+	   res.send("Hello world!");
 	});
 
-	app.get('/car',function(req,res){
-		
-		res.send("Car data requested.")
-	})
-
-
+app.get('/car',function(req,res){
+	
+	res.send("Car data requested.")
 })
-
 app.listen(3000);
+
+
+
+
+
+function testFunction(){
+	console.log("test")
+}
