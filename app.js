76afaf7bab6a4362fa2289a427cf9dda77b4e74c
ParetoYXE <35578020+ParetoYXE@users.noreply.github.com>
2020-09-17 12:25:40 -0700

    Update app.js

--- app.js
+++ app.js
@@ -1,16 +1,10 @@
-//Liam Iverson
-//EVOLVO
-//ExpressJS Server and Database Wrappe
-
-
-const express = require('express')
+var express = require('express');
 const bodyParser = require('body-parser')
-const app = express()
-const port = 3000
+var app = express();
+
+
 const MongoClient = require('mongodb').MongoClient
 
-
-//Database connection string
 
 conString = "mongodb+srv://admin:evolvo@cluster0.kzsnt.mongodb.net/carData?retryWrites=true&w=majority"
 
@@ -18,57 +12,23 @@ app.use(bodyParser.urlencoded({extended : true}))
 app.use(bodyParser.json())
 
 
-
-//Load and QueryDataBase
 MongoClient.connect(conString,{ useUnifiedTopology: true }).then(client =>{
 
     const db = client.db('CAR')
     const quotesCollection = db.collection('DASH')
 
-    //Load Initial Database
-    const cursor = db.collection('DASH').find().toArray()
-		.then(results =>{
-			console.log(results)
-		}).catch(error => console.error(error))
-
-
-    //Writes to DB everytime the form is updated
-    app.post('/car', (req, res) => {
-	  quotesCollection.insertOne(req.body)
-	    .then(result => {
-	      res.redirect('/')
-	    })
-	    .catch(error => console.error(error))
-	})
-
-    //Loads index and on refresh reads from the DB
-	app.get('/', (req, res) => {
-		res.sendFile(__dirname + '/index.html')
-		const cursor = db.collection('DASH').find().toArray()
-		.then(results =>{
-			console.log(results)
-		}).catch(error => console.error(error))
-	})
-
-
-	//When the Server recieves data it writes to the db
-	app.put('/car',(req,res)=>{
-		quotesCollection.insertOne(req.body)
-		res.sendFile(__dirname + '/index.html')
-		const cursor = db.collection('DASH').find().toArray()
-		.then(results =>{
-			console.log(results)
-		}).catch(error => console.error(error))
-	})
-
-
-	//Tell Server to begin listening on PORT 3000
-	app.listen(port, () => {
-	  console.log(`Example app listening at http://localhost:${port}`)
-	})
-
-
-	app.use(express.static('public'))
+    quotesCollection.insertOne({"name":"test","quote":"Written from load"})
 
 	})
 
+
+
+app.get('/', function(req, res){
+   res.send("Hello world!");
+});
+
+app.get('/car',function(req,res){
+	res.send("Car data requested.")
+})
+
+app.listen(3000);
