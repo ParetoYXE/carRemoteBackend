70954bd572a280d767e710b6c43b62af76f1ad36
ParetoYXE <35578020+ParetoYXE@users.noreply.github.com>
2020-09-17 11:49:54 -0700

    Update app.js

--- app.js
+++ app.js
@@ -3,10 +3,17 @@ const bodyParser = require('body-parser')
 var app = express();
 
 
-const MongoClient = require('mongodb').MongoClient
 
 
-conString = "mongodb+srv://admin:evolvo@cluster0.kzsnt.mongodb.net/carData?retryWrites=true&w=majority"
+//Set up mongoose connection
+var mongoose = require('mongoose');
+var mongoDB = 'mongodb+srv://admin:evolvo@cluster0.kzsnt.mongodb.net/carData?retryWrites=true&w=majority';
+mongoose.connect(mongoDB, { useNewUrlParser: true , useUnifiedTopology: true});
+var db = mongoose.connection;
+db.on('error', console.error.bind(console, 'MongoDB connection error:'));
+
+
+
 
 app.use(bodyParser.urlencoded({extended : true}))
 app.use(bodyParser.json())
@@ -15,8 +22,13 @@ app.use(bodyParser.json())
 
 
 app.get('/', function(req, res){
-	   response = testFunction()
-	   res.send(response);
+		db.collection('DASH').find().toArray(function (err, result) {
+	    if (err) throw err
+
+	    console.log(result)
+	  	
+	  	res.send(result);
+	  })
 	});
 
 app.get('/car',function(req,res){
@@ -26,28 +38,3 @@ app.get('/car',function(req,res){
 app.listen(3000);
 
 
-
-
-
-function testFunction(){
-	const data = []
-	MongoClient.connect(conString,{ useUnifiedTopology: true }).then(client =>{
-
-    const db = client.db('CAR')
-    const quotesCollection = db.collection('DASH')
-    //Load Initial Database
-    const cursor = db.collection('DASH').find().toArray()
-		.then(results =>{
-			data = results
-			return data
-		}).catch(error => console.error(error))
-
-
-
-
-    
-
-	})
-
-
-}
