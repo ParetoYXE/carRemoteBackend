310027fa0cc1d32d529eb6716c10b231055c15b0
ParetoYXE <35578020+ParetoYXE@users.noreply.github.com>
2020-09-17 15:20:41 -0700

    Update app.js

--- app.js
+++ app.js
@@ -1,28 +1,74 @@
-//Import the mongoose module
-var mongoose = require('mongoose');
-
-//Set up default mongoose connection
-var mongoDB = 'mongodb://159.203.5.95//my_database';
-mongoose.connect(mongoDB, { useNewUrlParser: true });
-
-//Get the default connection
-var db = mongoose.connection;
-
-//Bind connection to error event (to get notification of connection errors)
-db.on('error', console.error.bind(console, 'MongoDB connection error:'));
+//Liam Iverson
+//EVOLVO
+//ExpressJS Server and Database Wrappe
 
 
+const express = require('express')
+const bodyParser = require('body-parser')
+const app = express()
+const port = 3000
+const MongoClient = require('mongodb').MongoClient
 
 
-var express = require('express');
-var app = express();
+//Database connection string
 
-app.get('/', function(req, res){
-   res.send("Hello world!");
-});
+conString = "mongodb+srv://admin:evolvo@cluster0.kzsnt.mongodb.net/carData?retryWrites=true&w=majority"
 
-app.get('/car',function(req,res){
-	res.send("Car data requested.")
-})
+app.use(bodyParser.urlencoded({extended : true}))
+app.use(bodyParser.json())
 
-app.listen(3000);
+
+
+//Load and QueryDataBase
+MongoClient.connect(conString,{ useUnifiedTopology: true }).then(client =>{
+
+    const db = client.db('CAR')
+    const quotesCollection = db.collection('DASH')
+
+    //Load Initial Database
+    const cursor = db.collection('DASH').find().toArray()
+		.then(results =>{
+			console.log(results)
+		}).catch(error => console.error(error))
+
+
+    //Writes to DB everytime the form is updated
+    app.post('/car', (req, res) => {
+	  quotesCollection.insertOne(req.body)
+	    .then(result => {
+	      res.redirect('/')
+	    })
+	    .catch(error => console.error(error))
+	})
+
+    //Loads index and on refresh reads from the DB
+	app.get('/', (req, res) => {
+		res.sendFile(__dirname + '/index.html')
+		const cursor = db.collection('DASH').find().toArray()
+		.then(results =>{
+			console.log(results)
+		}).catch(error => console.error(error))
+	})
+
+
+	//When the Server recieves data it writes to the db
+	app.put('/car',(req,res)=>{
+		quotesCollection.insertOne(req.body)
+		res.sendFile(__dirname + '/index.html')
+		const cursor = db.collection('DASH').find().toArray()
+		.then(results =>{
+			console.log(results)
+		}).catch(error => console.error(error))
+	})
+
+
+	//Tell Server to begin listening on PORT 3000
+	app.listen(port, () => {
+	  console.log(`Example app listening at http://localhost:${port}`)
+	})
+
+
+	app.use(express.static('public'))
+
+	})
+
