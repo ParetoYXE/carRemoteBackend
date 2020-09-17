5f23cbd3290c3611993b7024b04900a5b6a5bc19
ParetoYXE <35578020+ParetoYXE@users.noreply.github.com>
2020-09-17 13:17:46 -0700

    Update app.js

--- app.js
+++ app.js
@@ -1,12 +1,34 @@
-var express = require('express');
-var app = express();
+const dbName = "Name";
 
-app.get('/', function(req, res){
-   res.send("Hello world!");
+const uri = "mongodb+srv://admin:evolvo@cluster0.kzsnt.mongodb.net/carData?retryWrites=true&w=majority";
+
+MongoClient.connect(
+  uri,
+  { useNewUrlParser: true, useUnifiedTopology: true },
+  (err, client) => {
+    if (err) {
+      console.log(err);
+      throw err;
+    }
+
+    console.log("Connected successfully to Mongodb");
+
+    const db = client.db(dbName);
+
+    app.locals.db = db;
+
+    const port = process.env.PORT || 3000;
+    app.listen(port, () => console.log(`Listening on port ${port}...`));
+  }
+);
+
+app.get("/test", async (req, res) => {
+  const db = req.app.locals.db;
+
+  const users = await db
+    .collection("DASH")
+    .find({})
+    .toArray();
+
+  res.send(users);
 });
-
-app.get('/car',function(req,res){
-	res.send("Car data requested")
-})
-
-app.listen(3000);
